8fd058187f6c48b655669e3ea755600f523dbad0
ya-ueda <ya.ueda@nskint.co.jp>
2022-07-19 14:15:17 +0900

    サーブレットのパスを通した

--- Webアプリケーション基礎Leeson1-3/src/main/java/pack/Servlet.java
+++ Webアプリケーション基礎Leeson1-3/src/main/java/pack/Servlet.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet
  */
-
+@WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
